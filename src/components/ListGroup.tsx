6a34c2bb90e5e709f876bbbe766b6af5144ea765
Patrikrizek <35512645+Patrikrizek@users.noreply.github.com>
2023-05-05 17:39:01 +0100

    Conditional Rendering Using Falsy
    Building Components

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -2,14 +2,10 @@ function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
   //   items = [];
 
-  const getMessage = () => {
-    return items.length === 0 ? <p>No items found</p> : null;
-  };
-
   return (
     <>
       <h1>List</h1>
-      {getMessage()}
+      {items.length === 0 && <p>No items found</p>}
       <ul className="list-group">
         {items.map((item) => (
           <li className="list-group-item" key={item}>

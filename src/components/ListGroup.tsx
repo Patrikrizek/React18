8c60158399f5bbbff9b15bd692979f422543bba5
Patrikrizek <35512645+Patrikrizek@users.noreply.github.com>
2023-05-06 15:01:55 +0100

    Passing Data via Props
    Building Components

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,14 +1,19 @@
 import { useState } from "react";
 
-function ListGroup() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+// Passing data via Props.
+// To define data type TS feature interface used bellow.
+interface Props {
+  items: string[];
+  heading: string;
+}
 
+function ListGroup({ items, heading }: Props) {
   // State Hook
   const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
       {items.length === 0 && <p>No items found</p>}
       <ul className="list-group">
         {items.map((item, index) => (

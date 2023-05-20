172f981157e5ffb2060bb79bec05528d52bf1e2a
Patrikrizek <35512645+Patrikrizek@users.noreply.github.com>
2023-05-20 17:01:38 +0100

    Updating Array of Objects
    Managing Component State

--- src/components/StateIntro.tsx
+++ src/components/StateIntro.tsx
@@ -2,22 +2,18 @@ import React from "react";
 import { useState } from "react";
 
 const StateIntro = () => {
-  const [tags, setTags] = useState(["happy", "cheerful"]);
+  const [bugs, setBugs] = useState([
+    { id: 1, title: "Bug 1", fixed: false },
+    { id: 2, title: "Bug 2", fixed: false },
+  ]);
 
   const handleClick = () => {
-    // Add
-    setTags([...tags, "exciting"]);
-
-    // Remove
-    setTags(tags.filter((tag) => tag !== "happy"));
-
-    // Update
-    setTags(tags.map((tag) => ("happy" ? "happiness" : tag)));
+    setBugs(bugs.map((bug) => (bug.id === 1 ? { ...bug, fixed: true } : bug)));
   };
 
   return (
     <div>
-      {tags}
+      {bugs[0].title + bugs[0].fixed}
       <button onClick={handleClick}>Click Me</button>
     </div>
   );

5169bf10885bd4a05625b3ce48e7e8d8ebb9df86
Patrikrizek <35512645+Patrikrizek@users.noreply.github.com>
2023-05-20 16:41:56 +0100

    Updating Arrays
    Managing Component State

--- src/components/StateIntro.tsx
+++ src/components/StateIntro.tsx
@@ -2,24 +2,22 @@ import React from "react";
 import { useState } from "react";
 
 const StateIntro = () => {
-  const [customer, setCustomer] = useState({
-    name: "Patrik",
-    address: {
-      city: "San Francisco",
-      zipCode: 94111,
-    },
-  });
+  const [tags, setTags] = useState(["happy", "cheerful"]);
 
   const handleClick = () => {
-    setCustomer({
-      ...customer,
-      address: { ...customer.address, zipCode: 94112 },
-    });
+    // Add
+    setTags([...tags, "exciting"]);
+
+    // Remove
+    setTags(tags.filter((tag) => tag !== "happy"));
+
+    // Update
+    setTags(tags.map((tag) => ("happy" ? "happiness" : tag)));
   };
 
   return (
     <div>
-      {customer.address.zipCode}
+      {tags}
       <button onClick={handleClick}>Click Me</button>
     </div>
   );

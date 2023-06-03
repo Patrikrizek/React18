709fc534954937d704aa0e0b43eb59bae19d62d2
Patrikrizek <35512645+Patrikrizek@users.noreply.github.com>
2023-06-03 13:22:06 +0100

    Effect Dependencies
    Connection to the Backend

---
+++ src/components/ProductList.tsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+
+const ProductList = ({ category }: { category: string }) => {
+  const [products, setProducts] = useState<string[]>([]);
+
+  useEffect(() => {
+    console.log("Fetching products in", category);
+    setProducts(["Clothing", "Household"]);
+  }, [category]);
+
+  return <div>ProductList</div>;
+};
+
+export default ProductList;

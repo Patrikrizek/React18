3c7458b95bcd744a1ad07ff964f59720ba08ad06
Patrikrizek <35512645+Patrikrizek@users.noreply.github.com>
2023-06-03 13:00:59 +0100

    Understanding the Effect Hook
    Connection to the Backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import { useEffect, useRef } from "react";
+
 function App() {
-  return <div></div>;
+  const ref = useRef<HTMLInputElement>(null);
+
+  // afterRender
+  useEffect(() => {
+    // Side effect
+    if (ref.current) ref.current.focus();
+  });
+
+  useEffect(() => {
+    document.title = "My App";
+  });
+
+  return (
+    <div>
+      <input ref={ref} type="text" className="from-control" />
+    </div>
+  );
 }
 
 export default App;

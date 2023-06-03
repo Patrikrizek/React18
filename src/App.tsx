862fc32df830e48498ac708285007430c59ee1b1
Patrikrizek <35512645+Patrikrizek@users.noreply.github.com>
2023-06-03 13:51:34 +0100

    Effect Clean Up
    Connection to the Backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,15 @@
-import { useState } from "react";
-import ProductList from "./components/ProductList";
+import { useEffect } from "react";
+
+const connect = () => console.log("Connecting");
+const disconnect = () => console.log("Disconnecting");
 
 function App() {
-  const [category, setCategory] = useState("");
-  return (
-    <div>
-      <select
-        className="form-select"
-        onChange={(event) => setCategory(event.target.value)}
-      >
-        <option value=""></option>
-        <option value="Clothing">Clothing</option>
-        <option value="Households">Households</option>
-      </select>
-      <ProductList category={category} />
-    </div>
-  );
+  useEffect(() => {
+    connect();
+
+    return () => disconnect();
+  });
+  return <div></div>;
 }
 
 export default App;

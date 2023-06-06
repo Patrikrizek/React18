f011c3327cd4789ca10a77f27d1bdc30085691d4
Patrikrizek <35512645+Patrikrizek@users.noreply.github.com>
2023-06-06 11:22:11 +0100

    Showing a Loading Indicator
    Connecting to the Backend

--- src/App.tsx
+++ src/App.tsx
@@ -9,18 +9,30 @@ interface User {
 function App() {
   const [users, setUsers] = useState<User[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
     const controller = new AbortController();
 
+    setLoading(true);
     axios
       .get<User[]>("https://jsonplaceholder.typicode.com/users", {
         signal: controller.signal,
       })
-      .then((response) => setUsers(response.data))
+      .then((response) => {
+        setUsers(response.data);
+        // proper way is setLoading(false) in .finally() only
+        setLoading(false);
+      })
       .catch((error) => {
         if (error instanceof CanceledError) return;
         setError(error.message);
+        // proper way is setLoading(false) in .finally() only
+        setLoading(false);
+      })
+      .finally(() => {
+        // This approach doesn't work in strict mode
+        setLoading(false);
       });
 
     return () => controller.abort();
@@ -29,6 +41,7 @@ function App() {
   return (
     <>
       {error && <p className="text-danger">{error}</p>}
+      {isLoading && <div className="spinner-border"></div>}
       <ul>
         {users.map((user) => (
           <li key={user.id}>{user.name}</li>
